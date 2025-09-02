b2f29135855736b946c7e4a5c296ab00a979505d
Strachuix <stanislawpokropek@gmail.com>
2025-09-02 21:12:59 +0200

    change port

--- api/sendMail.js
+++ api/sendMail.js
@@ -17,8 +17,8 @@ export default async function handler(req, res) {
   // Konfiguracja transportu SMTP (np. Gmail, Mailgun, SendGrid)
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS
